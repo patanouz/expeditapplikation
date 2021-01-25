ecc656b1b38803f0a55659ee839c59d481fde91c
Scarecrow <Scarecrow@LAPTOP-HE4ANLKG>
2021-01-25 11:53:03 +0100

    Lagt in button 2

--- PresentatationLayerExpApp/Form1.Designer.cs
+++ PresentatationLayerExpApp/Form1.Designer.cs
@@ -33,6 +33,7 @@ namespace PresentatationLayerExpApp
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -71,11 +72,21 @@ namespace PresentatationLayerExpApp
             this.listBox1.Size = new System.Drawing.Size(179, 95);
             this.listBox1.TabIndex = 3;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(267, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox1);
@@ -93,6 +104,7 @@ namespace PresentatationLayerExpApp
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
